d1498d0660b9540048f2c918a016f1ddc42e9274
Mayza <mayzabeel@gmail.com>
2017-01-18 00:45:24 -0300

    removing log

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -22,7 +22,6 @@
 			function successCallback (result) {
 				if (pageActual <= result.data.total_pages){
 					for (var i = result.data.results.length - 1; i >= 0; i--) {
-						console.log(pageActual);
 						$scope.upcoming.push(result.data.results[i]);
 					}
 					pageActual = pageActual + 1;
